0846172eb415fe53938be164762e96ca8e853bf5
Eugene Burkovskiy <gradam2001@gmail.com>
2022-10-01 21:15:02 +0300

    init start online-zoo-part2

--- online-zoo/script.js
+++ online-zoo/script.js
@@ -7,8 +7,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
     cards.forEach(card => {
         card.addEventListener('mouseenter', () => {
-            card.style.cssText = `transition: scale 0.6s; scale: 1.05;`;
-            card.querySelector('.choose__item-descr').style.cssText = `transition: bottom 0.6s; bottom: 0px;`;
+            if (window.screen.width > 768) {
+                card.style.cssText = `transition: scale 0.6s; scale: 1.05;`;
+                card.querySelector('.choose__item-descr').style.cssText = `transition: bottom 0.6s; bottom: 0px;`;
+            }
         });
     });
     cards.forEach(card => {
@@ -16,5 +18,16 @@ window.addEventListener('DOMContentLoaded', () => {
             card.style.cssText = `transition: scale 0.6s;`;
             card.querySelector('.choose__item-descr').style.cssText = `transition: bottom 0.6s;`;
         });
+    });
+    //hamburger
+    const burgerButton = document.querySelector('.hamburger'),
+        burgerContent = document.querySelector('.hamburger__content'),
+        burgerClose = document.querySelector('.hamburger__close');
+
+    burgerButton.addEventListener('click', () => {
+        burgerContent.style.left = '0';
+    });
+    burgerClose.addEventListener('click', () => {
+        burgerContent.style.left = '-100%';
     });
 });
